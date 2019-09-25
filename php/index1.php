bd1a94b3e74a3cf84a7f764326b4c88488690d3b
dat <dathalongbay@gmail.com>
2019-09-25 20:43:59 +0700

    1

---
+++ php/index1.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 9/25/2019
+ * Time: 8:41 PM
+ */
+
+?>
---
+++ php/index2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<?php
+echo "Chào mừng bạn đến với PHP";
+?>
+
+</body>
+</html>
