0d5e81dac1abafdefa86cf9ba423c8f23cd17151
dat <dathalongbay@gmail.com>
2019-09-28 18:35:50 +0700

    1

---
+++ php/index7.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 9/28/2019
+ * Time: 6:33 PM
+ */
+
+$a = 7;
+$b = 5;
+// toán tử . để nối chuỗi trong php
+echo "<br> tổng : " .  ($a + $b);
+echo "<br> hiệu : " .  ($a - $b);
+echo "<br> tích : " .  ($a * $b);
+echo "<br> thương : " .  ($a / $b);
+echo "<br> chia lấy dư : " .  ($a % $b);
