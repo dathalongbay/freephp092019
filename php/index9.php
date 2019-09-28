5de4f9fbd38052526c18dc87a9bc120057233633
dat <dathalongbay@gmail.com>
2019-09-28 18:53:36 +0700

    1

---
+++ php/index9.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 9/28/2019
+ * Time: 6:51 PM
+ */
+
+
+/**
+ * mệnh đề for
+ * $i = 1 biến đếm khoi đầu vòng lặp
+ * $i <= 20 điều kiện chạy của vòng lặp
+ * $i++ sự thay đổi của biến đếm sau mỗi lần chạy
+ */
+for($i = 1; $i <= 20; $i++) {
+    echo "<br>" . $i;
+}
