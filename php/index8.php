a705a1e9177f73d54eea356560b2a82c0ce9bdd6
dat <dathalongbay@gmail.com>
2019-09-28 18:46:08 +0700

    1

---
+++ php/index8.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 9/28/2019
+ * Time: 6:42 PM
+ */
+
+/**
+ * câu lệnh điều kiện trong php
+ */
+$tuoi = 7;
+if ($tuoi > 18) {
+    // nếu mệnh đề ($tuoi > 18) == true thì sẽ chạy vào mệnh đề sau if
+    echo "<br>" . "bạn đã trưởng thành";
+} else {
+    // nếu mệnh đề ($tuoi > 18) == false thì sẽ chạy vào mệnh đề sau else
+    echo "<br>" . "bạn chưa thành niên";
+}
