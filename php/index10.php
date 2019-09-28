4672a726080ffa498de3d202d44eda658880abda
dat <dathalongbay@gmail.com>
2019-09-28 19:00:26 +0700

    1

---
+++ php/index10.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 9/28/2019
+ * Time: 6:58 PM
+ */
+
+
+/**
+ * trước khi chạy while thì sé gán biến đếm $i = 1;
+ * điều kiện chạy vòng lặp while($i <= 10)
+ *
+ * sau mỗi lần chạy  $i++;
+ */
+$i = 1;
+while($i <= 10) {
+
+    echo "<br>" . $i;
+    
+    $i++;
+}
